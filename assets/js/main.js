30d0ac437d1a182c405daebcffbb14c870358418
Daniel Rocha <dsr.danielsrocha@gmail.com>
2023-11-21 09:52:50 -0300

    aplicado paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,40 @@
 
+const pokemonList = document.getElementById('pokemonList')
+const loadMore = document.getElementById('loadMore')
+const limit = 6
+let offset = 0;
 
 
-function convertPokemonToHtml(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-                    <div class="detail">
-                        <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type${type}">${type}</li>`).join('')}
-                            
-                        </ol>
-                        <img src="${pokemon.photo}" alt="${pokemon.name}">
-                    </div>
-                </li>
-    `
+function loadPokemonItens(offset, limit) {
+    function convertPokemonToHtml(pokemon) {
+        return `
+            <li class="pokemon ${pokemon.type}">
+                        <span class="number">#${pokemon.number}</span>
+                        <span class="name">${pokemon.name}</span>
+                        <div class="detail">
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type${type}">${type}</li>`).join('')}
+                                
+                            </ol>
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                    </li>
+        `
+    }
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtml).join('')
+        pokemonList.innerHTML += newHtml
+    
+    })
+
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
-    //pokemonList.innerHTML = newHtml
-
+loadMore.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
-
-
     
     
